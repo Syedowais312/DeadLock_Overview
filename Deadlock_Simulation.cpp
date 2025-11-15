6f1f70d82f4886d3eca44a9b3f2d4086cf0f6cf0
Syedowais312 <syedowais312sf@gmail.com>
2025-11-16 01:12:23 +0530

    added deacdlock simulation

--- Deadlock_Simulation.cpp
+++ Deadlock_Simulation.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+Using namespace std;
 
-std::mutex printerA;
-std::mutex printerB;
+ mutex printerA;
+ mutex printerB;
 
 void printDocument1() {
-    std::lock_guard<std::mutex> lockA(printerA);
-    std::cout << "Doc1: Using Printer A\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+     lock_guard< mutex> lockA(printerA);
+     cout << "Doc1: Using Printer A\n";
+     this_thread::sleep_for( chrono::milliseconds(50));
 
-    std::lock_guard<std::mutex> lockB(printerB);
-    std::cout << "Doc1: Using Printer B\n";
+     lock_guard< mutex> lockB(printerB);
+     cout << "Doc1: Using Printer B\n";
 }
 
 void printDocument2() {
-    std::lock_guard<std::mutex> lockB(printerB);
-    std::cout << "Doc2: Using Printer B\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+     lock_guard< mutex> lockB(printerB);
+     cout << "Doc2: Using Printer B\n";
+     this_thread::sleep_for( chrono::milliseconds(50));
 
-    std::lock_guard<std::mutex> lockA(printerA);
-    std::cout << "Doc2: Using Printer A\n";
+     lock_guard< mutex> lockA(printerA);
+     cout << "Doc2: Using Printer A\n";
 }
 
 int main() {
-    std::thread t1(printDocument1);
-    std::thread t2(printDocument2);
+     thread t1(printDocument1);
+     thread t2(printDocument2);
 
     t1.join();
     t2.join();
